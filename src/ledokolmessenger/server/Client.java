d1448a2e4587b9d3b789a973867446f4c14b00c2
X-Threc <79155566+X-Threc@users.noreply.github.com>
2021-05-26 19:17:37 +0300

    sleep test

--- src/ledokolmessenger/server/Client.java
+++ src/ledokolmessenger/server/Client.java
@@ -43,6 +43,8 @@ public class Client implements Runnable {
             outputStream.writeObject(db.getListFriends(this.clientName));
             while (true) {
                 SendableObject request = (SendableObject) inputStream.readObject();
+                
+                
                 if (request.getType().equals("addFriend")) {
                     ClientInfo request1 = (ClientInfo) request;
                     ClientInfo foundUser = db.addUser(request1.getClientName(), this.clientName);
@@ -54,25 +56,27 @@ public class Client implements Runnable {
                     } else {
                         outputStream.writeObject(new Respond("Respond", 200, "Пользователь " + foundUser.getClientName() + " добавлен в друзья", java.time.LocalDateTime.now()));
                         //outputStream.writeObject(foundUser);
-                    }
-                    
-                    
-                    
-                    
-                } else if (request.getType().equals("getOldMessages")) {
+                    }   
+                } 
+                
+                
+                else if (request.getType().equals("getOldMessages")) {
                     ClientInfo request1 = (ClientInfo) request;
                     MessageList oldMessages = db.getOldMessages(this.clientName, request1.getClientName());
-                    //outputStream.writeObject(new SendableObject("OldMessages"));
-                    outputStream.writeObject(oldMessages);
-                    
-                    
-                } else if (request.getType().equals("Message")) {
+                    if(oldMessages != null)
+                        outputStream.writeObject(oldMessages);  
+                } 
+                
+                
+                else if (request.getType().equals("Message")) {
                     Message message = (Message) request; //inputStream.readObject();
                     if (message.getMessage().equalsIgnoreCase("##session##end##")) {
                         break;
                     }
                     this.sendMessage(message);
                 }
+                
+                
             }
         } catch (IOException | ClassNotFoundException | SQLException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
